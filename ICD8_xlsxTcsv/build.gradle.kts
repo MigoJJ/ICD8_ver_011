b123684ec1b2832e17e258ef32d8ecba75f5b4ba
MigoJJ <63824334+MigoJJ@users.noreply.github.com>
2025-06-29 21:22:50 +0900

    Organize dependencies

--- ICD8_xlsxTcsv/build.gradle.kts
+++ ICD8_xlsxTcsv/build.gradle.kts
@@ -11,8 +11,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.apache.poi:poi-ooxml:5.2.3")
     implementation("mysql:mysql-connector-java:8.0.31")
+    implementation("org.apache.poi:poi-ooxml:5.2.3")
     implementation("org.slf4j:slf4j-api:2.0.0")
     runtimeOnly("ch.qos.logback:logback-classic:1.4.5")
     testImplementation("junit:junit:4.13.2")
